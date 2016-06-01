a65a8ce5a0481189d376995c122e582088194c77
John Richardson <johnmaster208@gmail.com>
2016-05-31 21:20:41 -0500

    Testing for outside origin.

--- sentiment.php
+++ sentiment.php
@@ -4,10 +4,14 @@ require_once('inc/class.sentiment.inc');
 
 $param = isset($_GET['request']) ? $_GET['request'] : '';
 
-#echo '$_GET["request"] param was ' . $param;
+echo '$_GET["request"] param was ' . $param;
+
+/**
 $s = new Sentiment($_REQUEST['request'],$_SERVER['HTTP_ORIGIN']);
 
+	echo '<pre>';
 	echo '<h4>Var dump for the request is: </h4>';
 	var_dump($s);
-
+	echo '</pre>';
+**/
 ?>
