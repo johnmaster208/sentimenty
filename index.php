2ba592ea57e1ac1395093617317c5dee1bca5528
John Richardson <jrichardson@digium.com>
2016-06-06 10:33:40 -0500

    Changing inc folder to includes.

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
 <?php
 echo "<h1>Sentiment API</h1>";
-	echo "<h3>Usage instructions</h3>";
+echo "<h3>Usage instructions</h3>";
 ?>
--- sentiment.php
+++ sentiment.php
@@ -1,17 +1,26 @@
 <?php
 
-require_once('inc/class.sentiment.inc');
+require_once('includes/class.sentiment.inc');
 
 $param = isset($_GET['request']) ? $_GET['request'] : '';
 
-echo '$_GET["request"] param was ' . $param;
+if($param == ''){
+	echo 'No request found. Please read the docs and try again.';
+} else {
+	echo "<h1>The request was: </h1>";
+	echo "<pre><h3>$param</h3></pre>";
+}
 
-/**
-$s = new Sentiment($_REQUEST['request'],$_SERVER['HTTP_ORIGIN']);
 
-	echo '<pre>';
-	echo '<h4>Var dump for the request is: </h4>';
-	var_dump($s);
-	echo '</pre>';
-**/
+
+
+
+// /**
+// $s = new Sentiment($_REQUEST['request'],$_SERVER['HTTP_ORIGIN']);
+
+// 	echo '<pre>';
+// 	echo '<h4>Var dump for the request is: </h4>';
+// 	var_dump($s);
+// 	echo '</pre>';
+// **/
 ?>
