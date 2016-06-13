11634ae64102ade618fb0f9a42cc0cddaa4c5192
John Richardson <jrichardson@digium.com>
2016-06-12 21:05:35 -0500

    Adding catch for abscence of ['HTTP_ORIGIN'].

--- sentimenty.php
+++ sentimenty.php
@@ -10,6 +10,11 @@ if($param == ''){
 	// echo "<h1>The request was: </h1>";
 	// echo "<pre><h3>$param</h3></pre>";
 
+	// Requests from the same server don't have a HTTP_ORIGIN header
+	if (!array_key_exists('HTTP_ORIGIN', $_SERVER)) {
+	    $_SERVER['HTTP_ORIGIN'] = $_SERVER['SERVER_NAME'];
+	}
+
 	try {
     	$s = new Sentimenty($_REQUEST['request'],$_SERVER['HTTP_ORIGIN']);
     	//echo $s->processAPI();
