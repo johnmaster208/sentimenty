f1a680e136ce9184c595cb11e41fa7dc190131e7
John Richardson <johnmaster208@gmail.com>
2016-06-20 09:56:24 -0500

    Adding viewport meta tag to demos.php

--- demos.php
+++ demos.php
@@ -2,6 +2,7 @@
 <html>
 <head>
 	<title>Sentimenty: Demos</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="stylesheet" href="css/bootstrap.min.css" type="text/css">
 </head>
   	<body>
