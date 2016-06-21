3e3ff7866dc75dc21a07b9a64b0643e7d28d03f9
John Richardson <johnmaster208@gmail.com>
2016-06-21 17:52:37 -0500

    Commenting out the unused services in index.php

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 	<head>
 		<title>Sentimenty</title>
 		<meta name="viewport" content="width=device-width, initial-scale=1">
-		<link rel="stylesheet" href="css/bootstrap.min.css" type="text/css">
+		<link rel="stylesheet" href="/css/bootstrap.min.css" type="text/css">
 	</head>
   	<body>
 	    <div class="container-fluid">
@@ -21,12 +21,15 @@
 		    <h2>How does it work?</h2>
 		     <p>Through generic POST requests to the following endpoint(s):</p>
 		    <div class="well well-lg">
-		      <h4>Textpool</h4>
-		      <p class="text-muted">textpool returns the emotional significance (or "e-sig" for short) of the text, broken into a distribution and ranked by an index.</p>
-		      <h5>Usage</h5>
-		      <code class="lead">/s/textpool/[text]</code>
+			<h4>Textpool</h4>
+	      	<p class="text-muted">textpool returns the emotional significance (or "e-sig" for short) of the text, broken into a distribution and ranked by an index.</p>
+	      	<h5>Usage</h5>
+	      	<code class="lead">/s/textpool/[text]</code>      
+	      	<div class="cta">
+	        <a href="/demo/textpool"><button class="btn btn-sm btn-primary">Try it out!</button></a>
+        	</div>
 		    </div>
-		    <div class="well well-lg">
+		    <!-- <div class="well well-lg">
 		      <h4>Textprobe</h4>
 		      <h5>COMING SOON!</h5>
 		    </div>
@@ -43,7 +46,7 @@
 		    <div class="well well-lg">
 		      <h4>Textprod</h4>
 		      <h5>COMING SOON!</h5>
-		    </div>
+		    </div> -->
 	  
 	      	<h2>Why did you name it Sentimenty?</h2>
 		    <div class="well">
@@ -51,18 +54,18 @@
 		        Mostly because the Sentiment.ly domain was taken already, but also because EmotionAPI sounded a little boring and too "Dashboard Confessionally"
 		      </p>
 		    </div>
-		    <h2>Ok, enough!</h2>
+		    <!-- <h2>Ok, enough!</h2>
 		    <div class="well">
 		      <p>
 		        Demo time? 
 		        <div class="cta">
-		        <a href="/demos.php"><button class="btn btn-lg btn-primary">Try it out!</button></a>
+		        <a href="/demo"><button class="btn btn-lg btn-primary">Try it out!</button></a>
 		        </div>
 		      </p>
-		    </div>
+		    </div> -->
 	    </div>
-	    <script type="text/javascript" src="js/jquery.min.js"></script>
-	    <script type="text/javascript" src="js/bootstrap.min.js"></script>
-	    <script type="text/javascript" src="js/demos.js"></script>
+	    <script type="text/javascript" src="/js/jquery.min.js"></script>
+	    <script type="text/javascript" src="/js/bootstrap.min.js"></script>
+	    <script type="text/javascript" src="/js/demos.js"></script>
 	</body>
 </html>
