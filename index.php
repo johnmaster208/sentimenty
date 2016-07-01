7278f3b73c9c340926f148381936a1e96ebb7baa
John Richardson <johnmaster208@gmail.com>
2016-06-30 20:47:34 -0500

    Adding updated styles and behaviors for demos.

--- index.php
+++ index.php
@@ -4,65 +4,50 @@
 		<title>Sentimenty</title>
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<link rel="stylesheet" href="/css/bootstrap.min.css" type="text/css">
+		<link rel="stylesheet" href="/css/fontawesome.min.css" type="text/css">
 	</head>
   	<body>
 	    <div class="container-fluid">
-		    <div id="cta" class="jumbotron">
+		    <div id="cta" class="jumbotron col-xs-12 col-sm-12 col-md-12">
 			    <h1 class="text-primary">Sentimenty</h1>
 			    <h4 class="text-muted">learning the emotional web</h4>
 		    </div>
-		    <h2>What is it?</h2>
-		    <div class="well well-lg">
-		      <p>
-		        Sentimenty is a service that learns emotion (<em>or, sentiment</em>) out of text.
-		      </p>
+		    <div class="intro col-xs-12 col-sm-12 col-md-12">
+		      <h2>What is it?</h2>
+		      <p>Sentimenty is a service that learns emotion (<em>or, sentiment</em>) out of text.</p>
+		      <h2>How does it work?</h2>
+		      <p>Through generic POST requests to the following endpoint(s):</p>
 		    </div>
-		    
-		    <h2>How does it work?</h2>
-		     <p>Through generic POST requests to the following endpoint(s):</p>
-		    <div class="well well-lg">
-			<h4>Textpool</h4>
-	      	<p class="text-muted">textpool returns the emotional significance (or "e-sig" for short) of the text, broken into a distribution and ranked by an index.</p>
-	      	<h5>Usage</h5>
-	      	<code class="lead">/s/textpool/[text]</code>      
-	      	<div class="cta">
-	        <a href="/demo/textpool"><button class="btn btn-sm btn-primary">Try it out!</button></a>
+		    <div class="service col-xs-12 col-sm-9">
+		      <section id="service-textpool" class="well well-sm row">
+			        <div class="details col-xs-12 col-sm-12">
+			           <h3>Textpool</h3>
+	  			      <p class="text-muted"> - returns the emotional significance (or "e-sig" for short) of the text.</p>
+	  			      <h4 class="lead"><code>/s/textpool/params</code></h4> 
+			        </div>
+		  	      	<div class="list-group col-xs-12 col-sm-12">
+		              <a href="#" class="list-group-item active">
+		                params
+		              </a>
+		              <a href="#" class="list-group-item">
+		                <h4>text</h4>
+		                <p class="small text-muted"> - <strong>Required.</strong> The submitted text or payload for processing any sentiments.</p>
+		              </a>
+		              <a href="#" class="list-group-item">
+		                <h4>format</h4>
+		                <p class="small text-muted"> - Optional. The submitted text or payload for processing any sentiments.</p>
+		              </a>
+		            </div>
+		            <div class="cta col-xs-12 col-sm-12">
+			            <a href="/demo/textpool">
+			              <button class="btn btn-md btn-primary">Try it out!</button>
+		              </a>
+	        	  	</div>	</section>
         	</div>
-		    </div>
-		    <!-- <div class="well well-lg">
-		      <h4>Textprobe</h4>
-		      <h5>COMING SOON!</h5>
-		    </div>
-		    <div class="well well-lg">
-		      <h4>Textpluck</h4>
-		      <h5>COMING SOON!</h5>
-		    </div>
-
-		    <div class="well well-lg">
-		      <h4>Textplot</h4>
-		      <h5>COMING SOON!</h5>
-		    </div>
-
-		    <div class="well well-lg">
-		      <h4>Textprod</h4>
-		      <h5>COMING SOON!</h5>
-		    </div> -->
-	  
-	      	<h2>Why did you name it Sentimenty?</h2>
-		    <div class="well">
-		      <p>
-		        Mostly because the Sentiment.ly domain was taken already, but also because EmotionAPI sounded a little boring and too "Dashboard Confessionally"
-		      </p>
-		    </div>
-		    <!-- <h2>Ok, enough!</h2>
-		    <div class="well">
-		      <p>
-		        Demo time? 
-		        <div class="cta">
-		        <a href="/demo"><button class="btn btn-lg btn-primary">Try it out!</button></a>
-		        </div>
-		      </p>
-		    </div> -->
+	      	<div class="nauseam col-xs-12 col-sm-12">
+	        	<h2>Why did you name it Sentimenty?</h2>
+		      	<p>Mostly because the Sentiment.ly domain was taken already, but also because EmotionAPI sounded a little boring and too "Dashboard Confessionally"</p>
+	      	</div>
 	    </div>
 	    <script type="text/javascript" src="/js/jquery.min.js"></script>
 	    <script type="text/javascript" src="/js/bootstrap.min.js"></script>
--- js/demos.js
+++ js/demos.js
@@ -1,33 +1,197 @@
 (function($){
+	//$(document).ready(function(){
+
+		function drawModal() {
+			var popup =  '<div id="results" class="modal fade text-center" role="dialog">';
+					popup += '<div class="modal-dialog">';
+						popup += '<div class="modal-content">';
+							popup += '<div class="modal-body">';
+							popup += '</div>';
+						popup += '</div>';
+					popup += '</div>';
+				popup += '</div>';
+			$('body').append(popup);
+		}
+
+		function drawHighChart(seriesData,chartType) {
+			var sentimentData = [];
+			for(var i = 0; i < seriesData.length; i++ ) {
+				var obj = {};
+				obj.name = seriesData[i]['word'];
+				obj.y = seriesData[i]['count'];
+				sentimentData.push(obj);
+			}
+	        // PIE CHART
+	        if(chartType === 'pie') {
+	        	$('#results div.modal-body').highcharts({
+		            chart: {
+		                plotBackgroundColor: null,
+		                plotBorderWidth: null,
+		                plotShadow: false,
+		                type: 'pie',
+		                credits: false
+		            },
+		            title: {
+		                text: 'Emotional Breakdown'
+		            },
+		            tooltip: {
+		                pointFormat: '<b>{point.y}</b>'
+		            },
+		            plotOptions: {
+		                pie: {
+		                    allowPointSelect: true,
+		                    cursor: 'pointer',
+		                    dataLabels: {
+		                        enabled: false
+		                    },
+		                    showInLegend: true
+		                }
+		            },
+		            series: [{
+		                name: 'Sentiment',
+		                colorByPoint: true,
+		                data: sentimentData
+		            }],
+		            credits: {
+		            	enabled: false
+		            }
+	        	});
+	        }//end if chart === pie    
+		}//end drawHighChart
+
+		function drawOutputDrawer(data,format) {
+
+			if(format === 'json') {
+				var jsonToggle =  '<div class="panel panel-default panel-primary">';
+						jsonToggle += '<div class="panel-heading" role="tab" id="json">';
+						jsonToggle += '<h4 class="panel-title">';
+							jsonToggle += '<a class="collapsed" role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseJSON">';
+								jsonToggle += 'JSON';
+							jsonToggle += '</a>';
+						jsonToggle += '</h4>';
+						jsonToggle += '</div>';
+						jsonToggle += '<div id="collapseJSON" class="panel-collapse collapse" role="tabpanel">';
+							jsonToggle += '<div class="panel-body">';
+							jsonToggle += '<pre><small>'+JSON.stringify(data,null,2)+'</small></pre>';
+							jsonToggle += '</div>';
+						jsonToggle += '</div>';
+					jsonToggle += '</div>';
+			} else if (format === 'xml') {
+				var xmlToggle =  '<div class="panel panel-default panel-primary">';
+					xmlToggle += '<div class="panel-heading" role="tab" id="xml">';
+					xmlToggle += '<h4 class="panel-title">';
+						xmlToggle += '<a class="collapsed" role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseXML" aria-expanded="false">';
+							xmlToggle += 'XML';
+						xmlToggle += '</a>';
+					xmlToggle += '</h4>';
+					xmlToggle += '</div>';
+					xmlToggle += '<div id="collapseXML" class="panel-collapse collapse" role="tabpanel">';
+						xmlToggle += '<div class="panel-body">';
+						var xml = new XMLSerializer().serializeToString(data).toString();
+						xmlToggle += '<small><xmp>'+xml+'</xmp></small>';
+						xmlToggle += '</div>';
+					xmlToggle += '</div>';
+				xmlToggle += '</div>';
+			}
 
 
-	$(document).ready(function(){
+			var output = '';
+			output += (typeof jsonToggle !== 'undefined') ? jsonToggle : '';
+			output += (typeof xmlToggle !== 'undefined') ? xmlToggle : '';
+
+			$('div#accordion').append(output);
+		}
+
+		function generateRandomPreloader(){
+			var loaders = [];
+			var preMsg1 =  '<div class="">';
+				preMsg1 += '<p class="lead">Oh boy, that\'s a tough one.</p>';
+				preMsg1 += '<p class="text-muted">Flossing our brains for awile...</p>';
+				preMsg1 += '<i class="fa fa-circle-o-notch fa-spin fa-5x fa-fw"></i>';
+				preMsg1 += '</div>';
+			var preMsg2 =  '<div clas="">';
+				preMsg2 += '<p class="lead">Wow, this should be fun!</p>';
+				preMsg2 += '<p class="text-muted">Now let\'s get to work...</p>';
+				preMsg2 += '<i class="fa fa-circle-o-notch fa-spin fa-5x fa-fw"></i>';
+				preMsg2 += '</div>';
+
+			loaders.push(preMsg1,preMsg2);
+			return loaders[Math.floor(Math.random()*loaders.length)];
+		}
+
 		$('button#textpool-generate').on('click',function(e) {
 			e.preventDefault();
 			var $form = $(this).closest('form');
 			var $textArea = $form.find('textarea#textpool-narrative');
-			var msg = "Sometime during the winter, little ducklings leave their mother in search of food. Without a doubt, the duckling's prominent predator is probably the vicious honey badger; He's very mean, and well, he just doesn't care.";
+			var msg = "Sometime during the winter, the frightened little ducklings leave their loving mother in search of food. Without a doubt, the duckling's prominent predator is the vicious honey badger; He's very awful, and well, he just doesn't care.";
 			$textArea.val(msg);
 			return false;
 		});
-
 		$('form#textpool-form-submit').submit(function(e) {
 			e.preventDefault();
-			var formData = $(this).serialize();
-			$.ajax({
-				'url': '/s/textpool',
-				'type': 'POST',
-				'data': formData,
-				'beforeSend': function(){
+			$('#results').remove();
+			//add the modal
+			drawModal();
+			//clear the output drawer
+			$('div#accordion').remove();
+			var $container = $('#results');
+			var $textpoolField = $(this).find('#textpool-narrative');
+			if($textpoolField.val().length == 0 || $textpoolField.val().length < 3){
+				$container.modal();
+				$container.find('div.modal-body')
+				.html('<div class="well well-lg"><p class="lead text-danger">Uh, oh. No sentiments loaded.</p><p class="lead"> Try adding some more text?</p></div>');
+				return false;
+			} else {
+				$container.modal();
+				var formData = $(this).serialize();
+				$container.on('show.bs.modal',function(e){
+					//if there is any leftover output from the last one, clear it.
+					$container.find('div.modal-body').html('');
+				});
+				$container.on('shown.bs.modal',function(e){
+					$.ajax({
+						'url': '/s/textpool',
+						'type': 'POST',
+						'data': formData,
+						'beforeSend': function(){
+							var loader = generateRandomPreloader();
+							$container.find('div.modal-body').html(loader);
+						},
+						'success': function(data) {
+							var reqType = this.data.match(/&format=([\w]*)/)[1];
+							$container.find('div.modal-body')
+							.html('<div class=""><h1 class="text-success">Finished!</h1><p class="text-muted">Go look on the demo page for your results!</p></div>')
+							.hide()
+							.fadeIn()
+							.animate({opacity: 1}, 100)
+							.animate({opacity: 0}, 100)
+							.animate({opacity: 1}, 100);
 
-				},
-				'success': function(data) {
-
-				}
-			});
+							console.log(this.data);
+							setTimeout(function(){
+								$('#results').modal('hide');
+							}, 2000);
+							$('div#accordion').remove();
+							var resultsPanel = '<div class="panel-group col-xs-12 col-md-6 row" id="accordion" role="tablist" aria-multiselectable="true"><button class="col-xs-12 col-md-6 btn btn-default btn-primary">CHART</button></div>';
+							$('#textpool-panel').append(resultsPanel);
+							drawOutputDrawer(data,reqType);
+							// var Data = data;
+							// console.log(Data);
+							// var chart = drawHighChart(Data,'pie');
+							// if(chart !== 'undefined'){
+							// 	$container.find('div.modal-body')
+							// 	.html(chart);
+							// }
+						}
+					});//end ajax
+				});
+			}//end else no text
+			
 			return false;
 		});
-	});
+
+
+	//});
 
 	
 
